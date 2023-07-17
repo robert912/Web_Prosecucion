ea944ba15af84dee48d5b00e0cc14a144f59ba71
Roberto Orellana Tamayo <roberto.orellana.t@usach.cl>
2023-07-16 22:41:02 -0400

    update

--- forms/contact.php
+++ forms/contact.php
@@ -53,6 +53,6 @@ try {
     //echo 'Message has been sent';
 
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "Mailer Error: {$mail->ErrorInfo}";
 }
 ?>
