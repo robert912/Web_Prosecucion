bef96e407ddfc104e7d47501d82737cc0de095a0
Roberto Orellana Tamayo <roberto.orellana.t@usach.cl>
2023-08-16 02:24:20 -0400

    adjuntar multi archivos

--- forms/contact.php
+++ forms/contact.php
@@ -38,8 +38,6 @@ try {
           $path = $_FILES["resume"]["tmp_name"][$i];
           $mail->addAttachment($path, $name);
       }
-    }else {
-      $mail->addAttachment($path, $name);
     }
     //Attachments
     //$name = $_FILES["resume"]["name"];
