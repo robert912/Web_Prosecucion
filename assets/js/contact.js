d39f450aca319f85e58a17d74b2d172241cca381
Roberto Orellana Tamayo <roberto.orellana.t@usach.cl>
2023-08-16 00:34:38 -0400

    adjuntar multi archivos

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,40 +1,11 @@
-
-
-
-
-
-
-
-
-var fileList = {};
-var fileArreglo = [];
-var historialArchivos = [];
-
-
-
-
 $( document ).ready( function () {
     adjuntar_archivos();
 });
 
-
-
 var adjuntar_archivos = () => {
     const dropArea = $(".drag-area");
     const dragText = dropArea.find("label");
-    const dragToUploadForm = $("#dragToUploadForm");
     const inputFile = dropArea.find("#inputFile");
-    let files = {};
-
-
-    /*dragToUploadForm.on("submit", function (e) {
-        console.log(files)
-        e.preventDefault();
-        if (files) {
-            this.submit();
-        }
-    });*/
-
 
     dropArea.on("mouseenter", function () {
         dropArea.addClass("active");
@@ -43,13 +14,12 @@ var adjuntar_archivos = () => {
         dropArea.removeClass("active");
     });
 
-
-    inputFile.on("change", function (e) {
+    inputFile.on("change", function (event) {
+        event.preventDefault();
         const files = this.files;
         dropArea.addClass("active");
         showFile(files);
     });
-
 
     dropArea.on("dragover", function (event) {
         event.preventDefault();
@@ -58,7 +28,8 @@ var adjuntar_archivos = () => {
     });
 
 
-    dropArea.on("dragleave", function () {
+    dropArea.on("dragleave", function (event) {
+        event.preventDefault();
         dropArea.removeClass("active");
         dragText.html('<i class="bi bi-cloud-upload" aria-hidden="true"></i> Arrastra y suelta para cargar archivos');
     });
@@ -66,52 +37,41 @@ var adjuntar_archivos = () => {
 
     dropArea.on("drop", function (event) {
         event.preventDefault();
-        files = event.originalEvent.dataTransfer.files;
+        let files = event.originalEvent.dataTransfer.files;
         showFile(files);
     });
 
 
     function showFile(files) {
-        $("#preview").html('')
+        $("#preview").html("")
         $.each(files, function (index, file) {
             let fileSize = file.size;
-            const maxSize = 200 * 1024 * 1024;
-            if (!fileExists(file)) {
-                if (fileSize <= maxSize) {
-                    fileList[Object.keys(fileList).length] = file
-                    fileArreglo = Object.values(fileList);
+            const maxSize = 100 * 1024 * 1024;
+            let fileType = file.type; //getting selected file type
+            let validExtensions = ["image/jpeg", "image/jpg", "image/png", "application/pdf"]; //adding some valid image extensions in array
+            if (fileSize <= maxSize){
+                if (validExtensions.includes(fileType)) {
+                //if user selected file is an image file
+                let fileReader = new FileReader(); //creating new FileReader object
+                fileReader.onload = () => {
+
                     const fileInfo = `<tr><td style="font-size: 12px; font-weight: 400; color:#495057;">${file.name}</td>
-<td style="font-size: 12px; font-weight: 400; color:#495057;">Tamaño: ${formatFileSize(fileSize)}</td>
-<td class ="btn-end"><button href="#" class='btn btn-raised btn-outline-primary btn-min-width mr-1 mb-0 btn_quitar' title='Eliminar archivo'><i class="bi bi-trash3"></i></button></td></tr>`;
+                                        <td style="font-size: 12px; font-weight: 400; color:#495057;">Tamaño: ${formatFileSize(fileSize)}</td>
+                                        </tr>`;
                     $("#preview").append(fileInfo);
+                };
+                fileReader.readAsDataURL(file);
                 } else {
-                   alert('El archivo excede el tamaño máximo permitido (200 MB).', 'Excede el tamaño máximo');
+                    alert('Solo se permiten archivos con extención pdf, jpge, jpg y png.', 'Formato archivo no permitido');
                 }
+            } else {
+                alert('El archivo excede el tamaño máximo permitido (100 MB).', 'Excede el tamaño máximo');
             }
         });
         dragText.html('<i class="bi bi-cloud-upload" aria-hidden="true"></i> Arrastra y suelta para cargar archivos');
         dropArea.removeClass("active");
     }
-
-
-    function fileExists(file) {
-        const fileListValues = Object.values(fileList);
-        return fileListValues.some((f) => f.name === file.name && f.size === file.size);
-    }
 }
-
-
-$("#preview").on("click", ".btn_quitar", function () {
-    fileList = {};
-    var fila = $(this).closest('tr');
-    var numeroFila = fila.index();
-    fileArreglo.splice(numeroFila, 1);
-    fileArreglo.forEach(function (elemento, indice) {
-        fileList[indice] = elemento;
-    });
-    $(this).closest("tr").remove();
-});
-
 
 function formatFileSize(bytes) {
     const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
