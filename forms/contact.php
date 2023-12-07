5126927bd09a91e34673fa17eff8109aae916ee9
Roberto Orellana Tamayo <roberto.orellana.t@usach.cl>
2023-12-07 09:15:02 -0300

    update form

--- forms/contact.php
+++ forms/contact.php
@@ -88,7 +88,9 @@ try {
             $mail->Body = str_replace("msjForm", $_POST['message'], $mail->Body);
 
             $mail->send();
-            header("Location: ../contact.html");
+            #header("Location: ../contact.html");
+            $output = "<div id='phppot-message' class='success'>Feedback received.</div>";
+            print $output;
             exit;
             //echo 'Message has been sent'; 
         }else{ 
